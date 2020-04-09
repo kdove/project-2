a88ba224751d504da670547c9cd4bf907051acac
Kyle Dove <kyle.dove@epdoffice.com>
2020-04-08 20:00:23 -0500

    test config on file

--- models/index.js
+++ models/index.js
@@ -9,11 +9,20 @@ var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env.JAWSDB_URL, { //[config.use_env_variable], {
-    "dialect": "mysql"
-  });
+//  var sequelize = new Sequelize(process.env.JAWSDB_URL, { //[config.use_env_variable], {
+//    "dialect": "mysql"
+ // });
+ var sequelize = new Sequelize(
+   config.database, 
+   config.username, 
+   config.password,
+  config);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
+  //var sequelize = new Sequelize(
+  //  "mysql://dttid9vau3gczr9x:bt99stnxnfsc7tqd@sulnwdk5uwjw1r2k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ujn0nqes4dk29jci", { //[config.use_env_variable], {
+   // "dialect": "mysql"
+  //});
 }
 
 fs
