c9ef79aaf25d3e51c42a3abcfb6bf40a2ed80d0d
Kyle Dove <kyle.dove@epdoffice.com>
2020-04-08 19:40:40 -0500

    Add dialect to sequelize initialization on model/index.js

--- models/index.js
+++ models/index.js
@@ -9,7 +9,9 @@ var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+  var sequelize = new Sequelize(process.env[config.use_env_variable], {
+    "dialect": "mysql"
+  });
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
