7e2ce2f9d24f981d17f1d10451f7144e2924bd25
ThomasJasonM1 <jason@call-em-all.com>
2020-04-08 22:00:04 -0500

    Updates to app.js to add a put for a ups

--- public/js/app.js
+++ public/js/app.js
@@ -1,66 +1,41 @@
-$(document).ready(function () {
-  $.get("/api/library").then(function (data) {
-    $(".upc").text(data.UPC);
-    $(".text").text(data.description);
-    $(".status").text(data.status);
-  });
-
-  $.get("/api/add").then(function (data) {
-    $(".upc").text(data.UPC);
-    $(".text").text(data.description);
-    $(".status").text(data.status);
-  });
-
-  var button = $("#data-id");
-  var upcAdd = $(".upc");
-  var descriptionAdd = $(".text");
-  var statusAdd = $(".status");
-
-
-  button.on("click", function (event) {
-    event.preventDefault();
-    var thisRow = $(this);
-    var upcData = {
-      upc: thisRow.$(".upc").val().trim(),
-      description: thisRow.$(".text").val().trim(),
-      status: thisRow.$(".status").val().trim()
-    };
-
-    changeStatusAdd(upcData.upc, upcData.description, upcData.status);
-    // $("#"+upcData.upc).hide();
-
-    changeStatusLibarary(upcData.upc, upcData.description, upcData.status);
-    // upcAdd.val("");
-    // descriptionAdd.val("");
-    // statusAdd.val("");
-  });
-
-  function changeStatusAdd(UPC, description, status) {
-    $.put("/api/add", {
-      UPC: UPC,
-      description: description,
-      status: status
+function changeStatus(UPC, description, status) {
+  $.put("/api/upc", {
+    Upc: UPC,
+    Description: description,
+    Active: status
+  })
+    .then(function () {
+      console.log("Success!");
     })
-      .then(function (res) {
-        console.log("Success!");
-      })
 
-      .catch(function (err) {
-        console.log(err);
-      });
-  }
+    .catch(function (err) {
+      console.log(err);
+    });
+}
 
-  function changeStatusLibrary(UPC, description, status) {
-    $.put("/api/library", {
-      UPC: UPC,
-      description: description,
-      status: status
-    })
-      .then(function () {
-        console.log("Success!");
-      })
-      .catch(function (err) {
-        console.log(err);
-      });
-  }
+$(document).on("blur", ".description-text", function (event) {
+  event.preventDefault();
+  console.log(event);
+  var thisRow = $(this);
+
+  var upcData = {
+    upc: thisRow.$(".upc").val().trim(),
+    description: thisRow.$(".text").val().trim(),
+    status: thisRow.$(".status").val().trim()
+  };
+
+  changeStatus(upcData.upc, upcData.description, upcData.status);
+});
+
+$(document).on("click", ".barcode-button", function (event) {
+  event.preventDefault();
+  var thisRow = $(this);
+  var upcData = {
+    upc: thisRow.$(".upc").val().trim(),
+    description: thisRow.$(".text").val().trim(),
+    status: thisRow.$(".status").val().trim()
+  };
+
+  changeStatus(upcData.upc, upcData.description, upcData.status);
+
 });
